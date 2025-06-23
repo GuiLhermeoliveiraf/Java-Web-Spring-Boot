44b8a632f214b373baab618e15ffc67e510cb255
GuiLhermeoliveira <gguimaraes647@gmail.com>
2025-06-22 22:32:28 -0300

    Resolvendo problema do GPT

--- PostgreSQL_e_Spring Data_JPA/Atividades/Screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ PostgreSQL_e_Spring Data_JPA/Atividades/Screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -1,22 +1,30 @@
 package br.com.alura.screenmatch.service;
 
-import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+
+import java.util.List;
 
 public class ConsultaChatGPT {
+
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("chave GPT");
+        OpenAiService service = new OpenAiService("Chave da APi");
 
+        ChatMessage mensagemUsuario = new ChatMessage("user", "Traduza para o português: " + texto);
 
-        CompletionRequest requisicao = CompletionRequest.builder()
-                .model("text-davinci-003")
-                .prompt("traduza para o português o texto: " + texto)
-                .maxTokens(1000)
+        ChatCompletionRequest requisicao = ChatCompletionRequest.builder()
+                .model("gpt-3.5-turbo")
+                .messages(List.of(mensagemUsuario))
                 .temperature(0.7)
+                .maxTokens(1000)
                 .build();
 
-
-        var resposta = service.createCompletion(requisicao);
-        return resposta.getChoices().get(0).getText();
+        return service.createChatCompletion(requisicao)
+                .getChoices()
+                .get(0)
+                .getMessage()
+                .getContent()
+                .trim();
     }
 }
