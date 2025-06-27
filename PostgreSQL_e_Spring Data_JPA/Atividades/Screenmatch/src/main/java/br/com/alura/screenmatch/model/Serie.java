45ee04f9ce5439e85dd594552deee2fe97054417
GuiLhermeoliveira <gguimaraes647@gmail.com>
2025-06-26 22:35:17 -0300

    Mapeando Entidades

--- PostgreSQL_e_Spring Data_JPA/Atividades/Screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ PostgreSQL_e_Spring Data_JPA/Atividades/Screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,17 +1,32 @@
 package br.com.alura.screenmatch.model;
-
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "Series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
+
     private Integer totalTemporadas;
+
     private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
+
     private String atores;
+
     private String imagem;
+
     private String sinopse;
 
     public Serie(DadosSerie dadosSerie){
@@ -22,6 +37,14 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.imagem = dadosSerie.imagem();
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
--- PostgreSQL_e_Spring Data_JPA/Atividades/Screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ PostgreSQL_e_Spring Data_JPA/Atividades/Screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -9,8 +9,7 @@ import java.util.List;
 public class ConsultaChatGPT {
 
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService(
-                "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");
+        OpenAiService service = new OpenAiService("");
 
         ChatMessage mensagemUsuario = new ChatMessage("user", "Traduza para o português: " + texto);
 
